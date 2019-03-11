72c80ea3c87443e9e9c7fdd6f840af60c7aa84e1
Arturo Morales <armoag@gmail.com>
2019-03-11 08:58:23 -0700

    added test method for insertion

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Shun;
 
@@ -17,6 +18,25 @@ namespace ShunUT
 
             var mysql = new MySqlDatabase(server, database, userID, password);
             mysql.Read("a");
+
+            var x = 1;
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var server = "wibsarlicencias.csqn2onotlww.us-east-1.rds.amazonaws.com";
+            var database = "Licenses";
+            var userID = "armoag";
+            var password = "Yadira00";
+
+            var mysql = new MySqlDatabase(server, database, userID, password);
+            var test1 = new Tuple<string, string>("LicenseKey", "ABC");
+            var test2 = new Tuple<string, string>("CurrentUser", "YadiraLeonaDormida");
+            var list = new List<Tuple<string, string>>();
+            list.Add(test1);
+            list.Add(test2);
+            mysql.Insert(list);
 
             var x = 1;
         }
